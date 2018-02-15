a4930736af734f2331e803969d7700a5306f19bf
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-02-15 10:09:19 +0100

    JS2 + Modal
    Dodanie pozostałych danych o pogodzie + częściowo modal

--- js/App.js
+++ js/App.js
@@ -11,18 +11,25 @@ const App = (_=> {
   }
 
   function showWeather(data, vars) {
-    const weather = data.current_observation;
+    const weather = data.current_observation.weather;
     const logoURL = data.current_observation.image.url;
     const weatherIconURL = data.current_observation.icon_url;
     const city = data.current_observation.display_location.city;
     const state = data.current_observation.display_location.state_name;
     const desc = data.current_observation.weather;
+    const humidity = data.current_observation.relative_humidity;
+    const dewpoint = data.current_observation.dewpoint_c;
+    const temp = data.current_observation.feelslike_c;
+    const wind_dir = data.current_observation.wind_dir;
+    const wind = data.current_observation.wind_kph;
+    const pressure = data.current_observation.pressure_mb;
+    const visibility = Math.round((data.current_observation.visibility_km));
 
-    console.log(weather);
+    console.log(data.current_observation);
     let windDir = '', weatherDesc = '';
 
     // Zamiana angielskich kierunków wiatru na polskie
-    switch(weather.wind_dir) {
+    switch(wind_dir) {
       case 'North': 
         windDir = 'Północny';
         break;
@@ -76,7 +83,7 @@ const App = (_=> {
         break;
     }
 
-    switch(weather.weather) {
+    switch(weather) {
       case 'Fog': 
         weatherDesc = 'Mgła';
         break;
@@ -104,11 +111,22 @@ const App = (_=> {
     vars.icon.setAttribute('src', weatherIconURL);
     vars.city.textContent = `${city}, ${state}`;
     vars.desc.textContent = `${weatherDesc}`;
+    vars.humidity.textContent = `${humidity}`;
+    vars.dewpoint.textContent = `${dewpoint}°C`;
+    vars.temp.textContent = `${temp}°C`;
+    vars.wind.textContent = `${windDir} o prędkości ${wind} km/h`;
+    vars.pressure.textContent = `${pressure} hPa`;
+    vars.visibility.textContent = `${visibility} km`;
+  }
+
+  function changeLocation() {
+    vars.modal.classList.add('on');
   }
 
   function init(_vars) {
     vars = _vars;
     getWeather();
+    vars.locationBtn.addEventListener('click', changeLocation);
   }
 
   return {
--- js/Variables.js
+++ js/Variables.js
@@ -3,7 +3,15 @@ function vars() {
     logo: document.querySelector('.weather-logo'),
     icon: document.querySelector('.weather-icon'),
     city: document.querySelector('.weather-location'),
-    desc: document.querySelector('.weather-desc')
+    desc: document.querySelector('.weather-desc'),
+    humidity: document.querySelector('.humidity'),
+    dewpoint: document.querySelector('.dewpoint'),
+    temp: document.querySelector('.feels-like'),
+    wind: document.querySelector('.wind'),
+    pressure: document.querySelector('.pressure'),
+    visibility: document.querySelector('.visibility'),
+    locationBtn: document.querySelector('.change-location'),
+    modal: document.querySelector('.modal')
   });
 }
 
